25c51ee370d84de67eaea2c5e89eec87d5ec8aa3
ragulhchc <ragulhchc@gmail.com>
2022-04-23 11:53:34 +0530

    commit

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser'); 
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const userRouter = require(__dirname + "/routes/user.routes");
+const adminRouter = require(__dirname + "/routes/admin.routes");
 
-const userRouter = require("./routes/user.routes");
-const adminRouter = require("./routes/admin.routes");
-
-const errorLogger = require("./utils/errorLogger");
+const errorLogger = require(__dirname + "/utils/errorLogger");
 
 const app = express();
 
-const MAX_UPLOAD_SIZE = '10mb';
+const MAX_UPLOAD_SIZE = "10mb";
 app.use(cors());
 
-app.use(bodyParser.json({limit:MAX_UPLOAD_SIZE}));
-app.use(bodyParser.urlencoded({extended:true, limit:MAX_UPLOAD_SIZE}))
-app.use('/user',userRouter);
-app.use('/admin',adminRouter);
+app.use(bodyParser.json({ limit: MAX_UPLOAD_SIZE }));
+app.use(bodyParser.urlencoded({ extended: true, limit: MAX_UPLOAD_SIZE }));
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
 app.use(errorLogger);
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
 
 // https://railwaywebservice.glitch.me/
 // mongodb+srv://ragulhchc:<password>@railway.cbxnl.mongodb.net/test
